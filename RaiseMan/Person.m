7ab04fde0827622451854ea31802ad76026d8fa0
Robert Gee <robert@Roberts-Mac-mini.local>
2018-02-23 16:15:22 -0600

    Added RaiseMan sorting and nil key handling

--- RaiseMan/Person.m
+++ RaiseMan/Person.m
@@ -23,4 +23,14 @@
     return self;
 }
 
+- (void)setNilValueForKey:(NSString *)key
+{
+    if ([key isEqual:@"expectedRaise"]) {
+        [self setExpectedRaise:0.0];
+    }
+    else {
+        [super setNilValueForKey:key];
+    }
+}
+
 @end
