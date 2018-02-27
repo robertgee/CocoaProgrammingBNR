6c96eac5794a23eabdc9b5688d0896798cfba637
Robert <rgee9207@gmail.com>
2018-02-26 21:07:39 -0600

    Chapter 9 added object observation

--- RaiseMan/RMDocument.m
+++ RaiseMan/RMDocument.m
@@ -7,12 +7,41 @@
 //
 
 #import "RMDocument.h"
+#import "Person.h"
 
 @interface RMDocument ()
 
 @end
 
 @implementation RMDocument
+
+// RMDocumentKVOContext enables this class to differentiate
+// between its KVO messages and those intended for a superclass.
+static void *RMDocumentKVOContext;
+
+- (void)startObservingPerson:(Person *)person
+{
+    [person addObserver:self
+             forKeyPath:@"personName"
+                options:NSKeyValueObservingOptionOld
+                context:&RMDocumentKVOContext];
+    
+    [person addObserver:self
+             forKeyPath:@"expectedRaise"
+                options:NSKeyValueObservingOptionOld
+                context:&RMDocumentKVOContext];
+}
+
+- (void)stopObservingPerson:(Person *)person
+{
+    [person removeObserver:self
+                forKeyPath:@"personName"
+                   context:&RMDocumentKVOContext];
+
+    [person removeObserver:self
+                forKeyPath:@"expectedRaise"
+                   context:&RMDocumentKVOContext];
+}
 
 - (instancetype)init {
     self = [super init];
@@ -59,5 +88,125 @@
         return;
     
     employees = a;
+    
+    for (Person *person in employees) {
+        [self stopObservingPerson:person];
+    }
 }
+
+- (void)insertObject:(Person *)p inEmployeesAtIndex:(NSUInteger)index
+{
+    NSLog(@"adding %@ to %@", p, employees);
+    // Add the inverse of this operation to the undo stack
+    NSUndoManager *undo = [self undoManager];
+    [[undo prepareWithInvocationTarget:self]
+                        removeObjectFromEmployeesAtIndex:index];
+    
+    if (![undo isUndoing]) {
+        [undo setActionName:@"Add Person"];
+    }
+    
+    // Add the Person to the array
+    [self startObservingPerson:p];
+    [employees insertObject:p atIndex:index];
+}
+
+- (void)removeObjectFromEmployeesAtIndex:(NSUInteger)index
+{
+    Person *p = [employees objectAtIndex:index];
+    NSLog(@"removing %@ from %@", p, employees);
+    // Add the inverse of this operation to the undo stack
+    NSUndoManager *undo = [self undoManager];
+    [[undo prepareWithInvocationTarget:self] insertObject:p
+                                       inEmployeesAtIndex:index];
+    
+    if (![undo isUndoing]) {
+        [undo setActionName:@"Remove Person"];
+    }
+    
+    [self stopObservingPerson:p];
+    [employees removeObjectAtIndex:index];
+}
+
+- (void)changeKeyPath:(NSString *)keyPath
+             ofObject:(id)obj
+              toValue:(id)newValue
+{
+    // setValue:forKeyPath: will cause the key-value observing method
+    // to be called, which takes care of the undo stuff
+    [obj setValue:newValue forKeyPath:keyPath];
+}
+
+- (void)observeValueForKeyPath:(NSString *)keyPath
+                      ofObject:(id)object
+                        change:(NSDictionary<NSKeyValueChangeKey,id> *)change
+                       context:(void *)context
+{
+    if (context != &RMDocumentKVOContext) {
+        // If the context does not match, this message
+        // must be intended for our superclass.
+        [super observeValueForKeyPath:keyPath
+                             ofObject:object
+                               change:change
+                              context:context];
+        return;
+    }
+    
+    NSUndoManager *undo = [self undoManager];
+    id oldValue = [change objectForKey:NSKeyValueChangeOldKey];
+    
+    // NSNull objects are used to represent nil in a dictionary
+    if (oldValue == [NSNull null]) {
+        oldValue = nil;
+    }
+    
+    NSLog(@"oldValue = %@", oldValue);
+    [[undo prepareWithInvocationTarget:self] changeKeyPath:keyPath ofObject:object toValue:oldValue];
+    
+    [undo setActionName:@"Edit"];
+}
+
+- (IBAction)createEmployee:(id)sender
+{
+    NSWindow *w = [tableView window];
+    
+    // Try to end any editing that is taking place
+    BOOL editingEnded = [w makeFirstResponder:w];
+    if (!editingEnded) {
+        NSLog(@"Unable to end editing");
+        return;
+    }
+    NSUndoManager *undo = [self undoManager];
+    
+    // Has an edit occurred already in this event?
+    if ([undo groupingLevel] > 0) {
+        // Close the last group
+        [undo endUndoGrouping];
+        // Open a new group
+        [undo beginUndoGrouping];
+    }
+    
+    // Create the object
+    Person *p = [employeeController newObject];
+    
+    // Add it to the content array of 'employeeController'
+    [employeeController addObject:p];
+    
+    // Re-sort (in case the user has sorted a column)
+    [employeeController rearrangeObjects];
+    
+    // Get the sorted array
+    NSArray *a = [employeeController arrangedObjects];
+    
+    // Find the object just added
+    NSUInteger row = [a indexOfObjectIdenticalTo:p];
+    NSLog(@"starting edit of %@ in row %lu", p, row);
+    
+    // Begin the dit in the first column
+    [tableView editColumn:0
+                      row:row
+                withEvent:nil
+                   select:YES];
+}
+
 @end
